45f25ae5ba0c1b136444fca8bbe522d3ad5a1beb
groot30 <sarvikashetty30@gmail.com>
2025-03-04 15:21:11 +0530

    Deleted all files

--- download_fer.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-
-# Download latest version of FER-2013 dataset
-path = kagglehub.dataset_download("msambare/fer2013")
-
-print("Path to dataset files:", path)
-# C:\Users\somis\.cache\kagglehub\datasets\msambare\fer2013\versions\1
